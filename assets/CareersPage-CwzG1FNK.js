dc2785046156449709082fc66d2654ea3ca87946
pavan-innocito <pavankumar.penta@innocito.com>
2025-09-02 18:10:14 +0530

    latest changes

--- assets/CareersPage-CwzG1FNK.js
+++
@@ -1,1 +0,0 @@
-import{j as e}from"./index-CPgILE_K.js";import{M as a}from"./MainLayout-EnxEKddd.js";const t=({data:r})=>e.jsx("div",{className:"min-h-screen overflow-x-hidden",children:e.jsx(a,{data:r?.careersPage})});export{t as default};
