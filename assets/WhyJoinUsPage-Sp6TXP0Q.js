ee806780a8e0cf9493892fce4a4724813ead2ac6
Kumar Raja Konna <kumar.konna@innocito.com>
2026-04-17 14:54:46 +0530

    added prasanna pic

---
+++ assets/WhyJoinUsPage-Sp6TXP0Q.js
@@ -0,0 +1,1 @@
+import{j as s}from"./index-CILSn1BD.js";import{M as e}from"./MainLayout-BTJOrC01.js";const t=({data:a})=>s.jsx("div",{className:"min-h-screen",children:s.jsx(e,{data:a?.whyJoinUsPage})});export{t as default};
